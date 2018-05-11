8534326990d7c08f822262d766a791792142931d
chaoshibin <chaoshibin@jrq.com>
2018-05-11 14:28:32 +0800

    重写http返回jackson序列化方式

--- ring-core/src/main/java/com/ring/core/config/spring/WebMvcConfigurer.java
+++ ring-core/src/main/java/com/ring/core/config/spring/WebMvcConfigurer.java
@@ -1,11 +1,21 @@
 package com.ring.core.config.spring;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author chaoshibin
@@ -28,5 +38,24 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     public BeanNameViewResolver beanNameViewResolver() {
         BeanNameViewResolver viewResolver = new BeanNameViewResolver();
         return viewResolver;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        //空对象不抛异常 e.g. handler{ }
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        //解析JSON时忽略未知属性
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+        // 设置中文编码格式
+        List<MediaType> list = new ArrayList<>();
+        list.add(MediaType.APPLICATION_JSON_UTF8);
+        converter.setSupportedMediaTypes(list);
+        super.configureMessageConverters(converters);
     }
 }
