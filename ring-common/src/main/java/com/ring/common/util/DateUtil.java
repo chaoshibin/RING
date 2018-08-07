3aaf0c2ea8e6c81ffbd1399d854a57ac2e9901d9
chaoshibinmf <hyn12345678>
2018-08-07 19:04:51 +0800

    添加时间工具类

--- ring-common/src/main/java/com/ring/common/util/DateUtil.java
+++ ring-common/src/main/java/com/ring/common/util/DateUtil.java
@@ -1,17 +1,17 @@
 package com.ring.common.util;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
-import java.util.Calendar;
 import java.util.Date;
 
 /**
  * @author chaoshibin
  */
-public final class DateUtil {
+public final class DateUtil extends DateUtils {
     private static final Logger LOG = LoggerFactory.getLogger(DateUtil.class);
     public static final String YYYY_MM_DD = "yyyy-MM-dd";
     public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
@@ -22,12 +22,11 @@ public final class DateUtil {
     public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
     public static final String YYYYMMDDHHMMSS_SSS = "yyyyMMddHHmmssSSS";
 
-
     private DateUtil() {
     }
 
     /**
-     * 默认格式
+     * 默认格式 yyyy-MM-dd HH:mm:ss
      *
      * @param date
      * @return
@@ -54,7 +53,7 @@ public final class DateUtil {
     }
 
     /**
-     * 解析日期
+     * 解析日期默认格式 yyyy-MM-dd HH:mm:ss
      *
      * @param dateStr Date
      * @return
@@ -86,89 +85,5 @@ public final class DateUtil {
             throw new RuntimeException(e);
         }
         return date;
-    }
-
-    /**
-     * 获取当日零点时间 yyyy-MM-dd 00:00:00
-     *
-     * @return Date
-     */
-    public static Date getTodayOfStart() {
-        Calendar instance = Calendar.getInstance();
-        instance.set(Calendar.HOUR_OF_DAY, 0);
-        instance.set(Calendar.MINUTE, 0);
-        instance.set(Calendar.SECOND, 0);
-        return instance.getTime();
-    }
-
-    /**
-     * 获取当日结束时间 yyyy-MM-dd 23:59:59
-     *
-     * @return Date
-     */
-    public static Date getTodayOfEnd() {
-        Calendar instance = Calendar.getInstance();
-        instance.set(Calendar.HOUR_OF_DAY, 23);
-        instance.set(Calendar.MINUTE, 59);
-        instance.set(Calendar.SECOND, 59);
-        return instance.getTime();
-    }
-
-    /**
-     * 获取某日零点时间 yyyy-MM-dd 00:00:00
-     *
-     * @return Date
-     */
-    public static Date getDayOfStart(Date date) {
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        instance.set(Calendar.HOUR_OF_DAY, 0);
-        instance.set(Calendar.MINUTE, 0);
-        instance.set(Calendar.SECOND, 0);
-        return instance.getTime();
-    }
-
-    /**
-     * 获取某日结束时间 yyyy-MM-dd 23:59:59
-     *
-     * @return Date
-     */
-    public static Date getDayOfEnd(Date date) {
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        instance.set(Calendar.HOUR_OF_DAY, 23);
-        instance.set(Calendar.MINUTE, 59);
-        instance.set(Calendar.SECOND, 59);
-        return instance.getTime();
-    }
-
-    /**
-     * 对时间域加值
-     *
-     * @param date   操作时间
-     * @param field  Calendar.YEAR/.../HOUR_OF_DAY/MINUTE
-     * @param amount 数值
-     * @return 取得时间
-     */
-    public static Date incrementField(Date date, int field, int amount) {
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        instance.add(field, amount);
-        return instance.getTime();
-    }
-
-    /**
-     * 对时间域设值
-     *
-     * @param date  操作时间
-     * @param field Calendar.YEAR/.../HOUR_OF_DAY/MINUTE
-     * @param value 数值
-     * @return 取得时间
-     */
-    public static Date setField(Date date, int field, int value) {
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(date);
-        instance.set(field, value);
-        return instance.getTime();
     }
 }
