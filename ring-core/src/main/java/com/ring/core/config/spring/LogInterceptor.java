5630788639aa58e14508bf87d3b54f13b1b44581
chaoshibinmf <hyn12345678>
2019-01-28 17:07:51 +0800

    日志拦截器

--- ring-core/src/main/java/com/ring/core/config/spring/LogInterceptor.java
+++ ring-core/src/main/java/com/ring/core/config/spring/LogInterceptor.java
@@ -2,11 +2,8 @@ package com.ring.core.config.spring;
 
 import com.ring.common.util.CodecUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
-import org.slf4j.spi.MDCAdapter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,7 +11,6 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.InetAddress;
-import java.util.Map;
 
 /**
  * @author chaoshibin
@@ -30,7 +26,7 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        if (MapUtils.isNotEmpty(httpServletRequest.getParameterMap())){
+        if (MapUtils.isNotEmpty(httpServletRequest.getParameterMap())) {
             String requestId = CodecUtil.createUUID();
             log.info("requestId:{}, clientIp:{}, X-Forwarded-For:{}", requestId, httpServletRequest.getRemoteAddr(),
                     httpServletRequest.getHeader("X-Forwarded-For"));
